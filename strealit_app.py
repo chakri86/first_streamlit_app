0491a200615ebec908b714b477e336e825f89cda
chakri86 <138947107+chakri86@users.noreply.github.com>
2023-07-07 19:53:21 -0500

    Update strealit_app.py
    added one more item in the menu

--- strealit_app.py
+++ strealit_app.py
@@ -2,6 +2,7 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast meanu')
-streamlit.text('Omega 3  & Blueberry oatmeal')
-streamlit.text('klae, spinach and rocket smoothie ')
-streamlit.text('har-boiled free-range eggs')
+streamlit.text('🥣 Omega 3  & Blueberry oatmeal')
+streamlit.text('🥗klae, spinach and rocket smoothie ')
+streamlit.text('🐔har-boiled free-range eggs')
+streamlit.text('🥑🍞 avacado toast')
