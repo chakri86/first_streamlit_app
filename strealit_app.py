446a6d849acf306e6d90073b7b074d0c4d7e6c30
chakri86 <138947107+chakri86@users.noreply.github.com>
2023-07-07 19:49:45 -0500

    Update strealit_app.py
    breakfast menu and items

--- strealit_app.py
+++ strealit_app.py
@@ -1,2 +1,7 @@
 import streamlit 
 streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast meanu')
+streamlit.text('Omega 3  & Blueberry oatmeal')
+streamlit.text('klae, spinach and rocket smoothie ')
+streamlit.text('har-boiled free-range eggs')
